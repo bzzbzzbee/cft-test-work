ae34d90a70a298cec2190e0d01a1f37d6ee3e39b
bzzbzzbee <chikipony666@gmail.com>
2021-09-09 01:27:52 +0700

    Adapter update

--- app/src/main/java/com/example/cft_test_work/adapters/CurrencyAdapter.kt
+++ app/src/main/java/com/example/cft_test_work/adapters/CurrencyAdapter.kt
@@ -5,52 +5,55 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import android.widget.Toast
-import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.cft_test_work.R
 import com.example.cft_test_work.data.entities.Currency
-import kotlin.properties.Delegates
 
-class CurrencyAdapter(private val dataSet: Array<Currency>) :
-    RecyclerView.Adapter<CurrencyAdapter.ViewHolder>() {
+class CurrencyAdapter(private val onClick: (Currency) -> Unit) :
+    ListAdapter<Currency, CurrencyAdapter.CurrencyViewHolder>(CurrencyDiffCallback) {
 
-    /**
-     * Provide a reference to the type of views that you are using
-     * (custom ViewHolder).
-     */
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    class CurrencyViewHolder(view: View, private val click: (Currency) -> Unit) :
+        RecyclerView.ViewHolder(view) {
         val nameText: TextView = view.findViewById(R.id.currencyNameTextView)
         val chCodeText: TextView = view.findViewById(R.id.сharCodeTextView)
         var id: Int = -1
+        var currentCurrency: Currency? = null
 
         init {
-            view.setOnClickListener {
-                Log.e("My id is : ", id.toString())
+            currentCurrency?.let {
+                click
             }
+        }
+
+        fun bind(currency: Currency) {
+            currentCurrency = currency
+
+            nameText.text = currency.name
+            chCodeText.text = currency.charCode
+            id = currency.cId
         }
     }
 
-    // Create new views (invoked by the layout manager)
-    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
-        // Create a new view, which defines the UI of the list item
-        val view = LayoutInflater.from(viewGroup.context)
-            .inflate(R.layout.recycle_view_item, viewGroup, false)
-
-        return ViewHolder(view)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CurrencyViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.recycle_view_item, parent, false)
+        return CurrencyViewHolder(view, onClick)
     }
 
-    // Replace the contents of a view (invoked by the layout manager)
-    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
-
-        // Get element from your dataset at this position and replace the
-        // contents of the view with that element
-        viewHolder.nameText.text = dataSet[position].name
-        viewHolder.chCodeText.text = dataSet[position].charCode
-        viewHolder.id = dataSet[position].cId
+    override fun onBindViewHolder(viewHolder: CurrencyViewHolder, position: Int) {
+        val currency = getItem(position)
+        viewHolder.bind(currency)
     }
 
-    // Return the size of your dataset (invoked by the layout manager)
-    override fun getItemCount() = dataSet.size
+    object CurrencyDiffCallback : DiffUtil.ItemCallback<Currency>() {
+        override fun areItemsTheSame(oldItem: Currency, newItem: Currency): Boolean {
+            return oldItem == newItem
+        }
 
+        override fun areContentsTheSame(oldItem: Currency, newItem: Currency): Boolean {
+            return oldItem.id == newItem.id
+        }
+    }
 }
